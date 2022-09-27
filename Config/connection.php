b478ca46fe613eeaf14efcc37275ba4217616a83
Rohan Parmar <rohan.nitsan@gmail.com>
2022-09-27 12:43:13 +0530

    Database Connection

---
+++ Config/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "blog";
+
+    public function __construct()
+    {
+        $conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        if ($conn->connect_error) {
+            die("Connection Error: " . $conn->connect_error);
+        }
+    }
+}
+$obj = new Database();
