bb6485a6eed6219e5cbdab38c963c4e7f9004e39
Rohan Parmar <rohan.nitsan@gmail.com>
2022-09-27 12:58:01 +0530

    Add CSS & JS

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./Assets/CSS/bootstrap.min.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+
+    <!-- Bootstrap JS   -->
+    <script src="./Assets/JS/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
